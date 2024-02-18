4e7a47f496816c315899c1e8020d92f3a00e1ed5
saideepoki <deeps2657@gmail.com>
2024-02-18 13:39:09 +0000

    added first bash script file

---
+++ firstscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# First script
+echo "This is my first script"
+echo
+
+echo "############################"
+
+echo "The uptime of the system is: "
+uptime
+
+
+echo "############################"
+echo "The memory utilization of the system is: "
+free -m
+
+echo "############################"
+echo "The disk utilization of the system is: "
+df -h
+
+echo "End of the script"
