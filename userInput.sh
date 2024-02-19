d9e1bc8618fdd48bddb960d07a6973cfa84eeb95
saideepoki <deeps2657@gmail.com>
2024-02-19 19:20:27 +0000

    added script which takes user input

---
+++ userInput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter your skill: " SKILL
+
+echo "$SKILL is in high demand in IT industry"
+
+read -p "Enter your username: " USR
+read -sp "Enter your password: " PASS
+echo
+echo
+echo "Successful login by $USR.."
+
+
