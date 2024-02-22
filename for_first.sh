1282ac30169d22828b85efce2cee9a2413fcb523
saideepoki <deeps2657@gmail.com>
2024-02-22 09:10:46 +0000

    first for loop script

---
+++ for_first.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for VAR1 in java .net python ruby javascript
+do
+ echo "Looping..."
+ echo
+ sleep 1
+ echo "###############################"
+ echo "VAR1 is $VAR1"
+ echo "###############################"
+ echo
+done
+
